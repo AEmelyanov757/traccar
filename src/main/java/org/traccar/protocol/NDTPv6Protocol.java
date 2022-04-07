a71cc68e74a6d73a8710e3c781fdb502e076196b
AEmelyanov757 <aemelyanov757@gmail.com>
2022-04-08 09:18:27 +1200

    Создал проект на GIT

---
+++ src/main/java/org/traccar/protocol/NDTPv6Protocol.java
@@ -0,0 +1,23 @@
+/*
+ * 2020 - NDTP v6 Protocol
+ */
+package org.traccar.protocol;
+
+import org.traccar.BaseProtocol;
+import org.traccar.PipelineBuilder;
+import org.traccar.TrackerServer;
+
+public class NDTPv6Protocol extends BaseProtocol {
+
+  public NDTPv6Protocol() {
+    addServer(
+      new TrackerServer(false, getName()) {
+
+        @Override
+        protected void addProtocolHandlers(PipelineBuilder pipeline) {
+          pipeline.addLast(new NDTPv6ProtocolDecoder(NDTPv6Protocol.this));
+        }
+      }
+    );
+  }
+}
